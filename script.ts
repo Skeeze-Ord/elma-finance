727af4b5d01ab8b20b1133f614ede59cd262ef2c
Arman <smoozy.gallardo@gmail.com>
2024-06-19 19:50:31 +0500

    save

--- script.ts
+++ script.ts
@@ -38,10 +38,13 @@ function getMonthlyAccruals() {
 }
 
 async function onProjects() {
-    const [accruals, tabels] =  await Promise.all([
+    // Входящий период
+    const inputYear = Context.data.year?.name;
+
+    const [accruals, tabels] = await Promise.all([
         Global.ns.finance.app.accruals.search().size(1000).all(),
         Global.ns.finance.app.tabels.search().size(1000).all()
-    ])
+    ]);
 
     // Объект для хранения сумм по периодам
     const accrualsMap: { [key: string]: IAccrualsTable } = {};
@@ -56,65 +59,78 @@ async function onProjects() {
                 && !accrual.data.__deletedAt
                 && accrual.data.__status
                 && (accrual.data.__status.code === 'report' || accrual.data.__status.code === 'end')) {
+                const accrual_period = await accrual.data.period?.fetch();
 
-                // Проект, задача, задание, период, специализация
-                const [accrual_task, accrual_project, accrual_projectTask, accrual_period, specialization] = await Promise.all([
-                    accrual.data.zadanie?.fetch(),
-                    accrual.data.project?.fetch(),
-                    accrual.data.zadanie?.fetch().then(task => task.data.project_task?.fetch()),
-                    accrual.data.period?.fetch(),
-                    accrual.data.zadanie?.fetch().then(task => task.data.specializaciya?.fetch())
-                ]);
-
-                // Рассчитываем суммы
-                const accrued = accrual.data.sum ? accrual.data.sum.asFloat() : 0;
-                const ndfl = accrual.data.ndfl ? accrual.data.ndfl.asFloat() : 0;
-                const total = accrual.data.total ? accrual.data.total.asFloat() : 0;
-                const prepaid = total / 2;
-                const salary = total - prepaid;
-                const paid = 0;
-                const difference = 0;
-
-                // Поля для объекта
+                // Получаем год из периода начисления
                 const periodName = accrual_period?.data.__name || "Без периода";
-                const specializationName = specialization?.data.__name || "Нет специализации";
-                const projectTaskName = accrual_projectTask?.data.__name || "Нет проектной задачи";
-                const taskName = accrual_task?.data.__name || "Нет задачи";
-                const roleName = accrual.data.role ? accrual.data.role.name : "";
+                const periodTrimmed = periodName.trim();
+                const periodMatch = periodTrimmed.match(/\d{4}$/);
 
-                // Если период уже существует в accrualsMap, суммируем значения
-                if (accrualsMap[periodName]) {
-                    accrualsMap[periodName].accrued += total;
-                    accrualsMap[periodName].prepaid += prepaid;
-                    accrualsMap[periodName].salary += salary;
-                    accrualsMap[periodName].paid += paid;
-                    accrualsMap[periodName].difference += difference;
-                } else {
-                    // Если периода нет, создаем новую запись
-                    accrualsMap[periodName] = {
+                if (!periodMatch) {
+                    continue;
+                }
+
+                // Получаем год из периода начисления
+                const year = parseInt(periodMatch[0], 10);
+
+                if (year.toString() === inputYear) {
+                    // Проект, задача, задание, специализация
+                    const [accrual_task, accrual_project, accrual_projectTask, specialization] = await Promise.all([
+                        accrual.data.zadanie?.fetch(),
+                        accrual.data.project?.fetch(),
+                        accrual.data.zadanie?.fetch().then(task => task.data.project_task?.fetch()),
+                        accrual.data.zadanie?.fetch().then(task => task.data.specializaciya?.fetch())
+                    ]);
+
+                    // Рассчитываем суммы
+                    const accrued = accrual.data.sum ? accrual.data.sum.asFloat() : 0;
+                    const ndfl = accrual.data.ndfl ? accrual.data.ndfl.asFloat() : 0;
+                    const total = accrual.data.total ? accrual.data.total.asFloat() : 0;
+                    const prepaid = total / 2;
+                    const salary = total - prepaid;
+                    const paid = 0;
+                    const difference = 0;
+
+                    // Поля для объекта
+                    const specializationName = specialization?.data.__name || "Нет специализации";
+                    const projectTaskName = accrual_projectTask?.data.__name || "Нет проектной задачи";
+                    const taskName = accrual_task?.data.__name || "Нет задачи";
+                    const roleName = accrual.data.role ? accrual.data.role.name : "";
+
+                    // Если период уже существует в accrualsMap, суммируем значения
+                    if (accrualsMap[periodName]) {
+                        accrualsMap[periodName].accrued += total;
+                        accrualsMap[periodName].prepaid += prepaid;
+                        accrualsMap[periodName].salary += salary;
+                        accrualsMap[periodName].paid += paid;
+                        accrualsMap[periodName].difference += difference;
+                    } else {
+                        // Если периода нет, создаем новую запись
+                        accrualsMap[periodName] = {
+                            period: periodName,
+                            accrued: total,
+                            prepaid: prepaid,
+                            salary: salary,
+                            paid: paid,
+                            difference: difference
+                        };
+                    }
+
+                    // Детальное отображение для второй таблицы
+                    const detail: IMonthlyAccrualsTable = {
                         period: periodName,
-                        accrued: total,
-                        prepaid: prepaid,
-                        salary: salary,
-                        paid: paid,
-                        difference: difference
-                    };
-                }
+                        specialization: specializationName,
+                        client_project: printCustomer(accrual.data.client_name, accrual_project?.data.__name),
+                        projectTask: projectTaskName,
+                        task: taskName,
+                        role: roleName,
+                        accrued: convertToFixed(accrued),
+                        ndfl: convertToFixed(ndfl),
+                        total: convertToFixed(total),
+                    }
 
-                // Детальное отображение для второй таблицы
-                const detail: IMonthlyAccrualsTable = {
-                    period: periodName,
-                    specialization: specializationName,
-                    client_project: printCustomer(accrual.data.client_name, accrual_project?.data.__name),
-                    projectTask: projectTaskName,
-                    task: taskName,
-                    role: roleName,
-                    accrued: convertToFixed(accrued),
-                    ndfl: convertToFixed(ndfl),
-                    total: convertToFixed(total),
+                    monthlyAccrualsList.push(detail);
                 }
-
-                monthlyAccrualsList.push(detail);
             }
         }
     }
@@ -129,7 +145,6 @@ async function onProjects() {
         return a.period.localeCompare(b.period);
     });
 }
-
 
 // Убрать знаки после запятой
 function convertToFixed(item: number):number {
